e71294c763811a20a789c7237d9d33b5b2f228ec
ehek01 <whdgus0515@gmail.com>
2022-08-16 22:10:22 +0900

    develop branch add !!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        System.out.println("develop branch add");
         IndexMenu menu = new IndexMenu();
         menu.mainMenu();
     }
