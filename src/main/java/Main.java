0a5c51be2e0ba83edfae8dae865c7e621bcfd68f
ehek01 <whdgus0515@gmail.com>
2022-08-16 22:27:07 +0900

    push test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("develop branch add");
+        System.out.println("push test");
         IndexMenu menu = new IndexMenu();
         menu.mainMenu();
     }
