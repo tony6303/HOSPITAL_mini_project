fe4f60a380444ebe52230a30e700956056006305
ehek01 <whdgus0515@gmail.com>
2022-08-16 23:04:05 +0900

    delete 재고

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("push test");
+        System.out.println("delete test");
         IndexMenu menu = new IndexMenu();
         menu.mainMenu();
     }
