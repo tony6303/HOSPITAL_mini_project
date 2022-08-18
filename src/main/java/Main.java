4e24b06bffa733f82103af2ae378d9e1f9b3c287
dudwns981 <choiyj981@gmail.com>
2022-08-18 15:47:24 +0900

    첫커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import com.example.utils.AsciiArtUtils;
 public class Main {
     public static void main(String[] args) throws SQLException {
 //        AsciiArtUtils.show("test.txt");
-    	// 연습용
+    	// 연습으로
         AsciiArtUtils.show("hospital.txt");
         IndexMenu menu = new IndexMenu();
         menu.mainMenu();
